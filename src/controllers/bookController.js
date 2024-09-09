a071034b56cc0a66b5a99bb5a42b74dc414a7ce8
sivuyilemtwetwe <sivuyilemtwetwe@gmail.com>
2024-09-09 17:36:26 +0200

    CRUD functions

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -19,3 +19,42 @@ exports.getAllBooks = async (req , res) => {
         res.status(500).json({error: error.message});
     }
 }
+
+exports.getBook = async (req , res) =>{
+    try {
+        const {id} = req.params
+        const book = await Book.findById(id)
+        if(!book) return res.status(404).json({message: "Book not found"})
+        res.status(200).json(book)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+}}
+
+exports.deleteAllBooks = async (req , res) =>{
+    try {
+        await Book.deleteMany()
+        res.status(200).json({message: "All books deleted"})
+    } catch (error) {
+        res.status(500).json({error: error.message});
+}
+}
+
+exports.deleteBook = async ( req , res)=> {
+    try {
+        const {id} = req.params
+        const book = await Book.findByIdAndDelete(id)
+        if(!book) return res.status(404).json({message: "Book not found"})
+        res.status(200).json({message: "Book deleted"})
+    } catch (error) {
+        res.status(500).json({error: error.message});
+}}
+
+exports.updateBook = async (req , res) => {
+    try {
+        const {id} = req.params
+        const book = await Book.findByIdAndUpdate(id, req.body, {new: true})
+        if(!book) return res.status(404).json({message: "Book not found"})
+        res.status(200).json(book)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+}}
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,6 +5,8 @@ const bookController =  require("../controllers/bookController");
 
 router.post("/" , bookController.createBook);
 router.get("/" , bookController.getAllBooks);
-
-
+router.get("/:id" , bookController.getBook)
+router.delete("/" , bookController.deleteAllBooks)
+router.delete("/:id" , bookController.deleteBook)
+router.patch("/:id" , bookController.updateBook)
 module.exports = router;
